2b425183f22c6366da27518898ca2ac61fc55c93
Sibel <sbaslamisli@yahoo.com>
2020-01-13 08:53:52 -0500

    split panel is working

--- splitpanel/src/App.js
+++ splitpanel/src/App.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import YourComponent from '../src/components/splitpanel/index';
 
 function App() {
+
+const [screen, setScreen] = useState();
+
+useEffect(()=>{
+
+}, []);
+
+function splitHandle(){
+  setScreen(YourComponent)
+}
+
+function splitHandleBack(){
+  setScreen(!YourComponent)
+}
   return (
     <div className="App">
       Hello
-      <button>Click me</button>
-     <div className="mainScreen">
-        <YourComponent/>
+      <button onClick={splitHandle}>Click me</button>
+      <button onClick={splitHandleBack}>Click Back</button>
+
+      <div className= "screen">
+        {screen}
+      </div>
      </div>
-    </div>
   );
 }
 
